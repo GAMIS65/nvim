b0928a8ce937ef7a79fa354d40c4b47c61d24a69
GAMIS65 <ivicko05@gmail.com>
2025-10-03 16:52:27 +0200

    theme

--- lua/kickstart/plugins/colors.lua
+++ lua/kickstart/plugins/colors.lua
@@ -1,16 +1,8 @@
-function ColorMyPencils(color)
-  color = color or 'rose-pine'
-  vim.cmd.colorscheme(color)
-
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-end
-
 return {
   {
-    'rebelot/kanagawa.nvim',
+    'arzg/vim-colors-xcode',
     config = function()
-      vim.cmd("colorscheme kanagawa-wave")
+      vim.cmd("colorscheme xcodedarkhc")
     end,
   },
 }
